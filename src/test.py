337926dbf6c0444ba4555051fdb4ba1bead22ad6
Naheed <naheedcse@gmail.com>
2017-11-25 21:23:04 +0800

    test cases for strong witness filtration

--- src/test.py
+++ src/test.py
@@ -30,9 +30,10 @@ def testRandomSelector():
     matrixofpoints_inR2 = np.random.multivariate_normal(mean, cov, 100)
     p = pc.PointCloud(matrixofpoints_inR2)
     # Create Selector
-    s = sel.PointCloudSelector(p, 2, "RandomSelector")
+    s = sel.PointCloudSelector(p, 5, "RandomSelector")
     print s.getLandmarkPoints().Points
 
+    print s.getdistance_subsetstoPointcloud()
 
 def testRandomSelectorDistanceMetricio():
     from DistanceMetricinput import DistanceMetricIn
@@ -44,8 +45,39 @@ def testRandomSelectorDistanceMetricio():
     print s.getLandmarkPoints()
 
 
+def testMaxdist():
+    from DistanceMetricinput import DistanceMetricIn
+    d_in = DistanceMetricIn('/Users/naheed/PycharmProjects/PersistenceHomology/data/dmateight.mat',
+                            varname='dist_eight', dimension=2)
+    print d_in.size, d_in.dim
+
+    s = sel.MetricSelector(d_in, 4, "RandomSelector")
+    print s.getdistance_subsetstoPointcloud()
+
+
+def testWitnessStream():
+    from WitnessFiltration import WitnessStream
+    # mean = [0, 0]
+    # cov = [[1, 0], [0, 1]]
+    # matrixofpoints_inR2 = np.random.multivariate_normal(mean, cov, 100)
+    # p = pc.PointCloud(matrixofpoints_inR2)
+    filename = '/Users/naheed/PycharmProjects/PersistenceHomology/data/eight.mat'
+    p = pc.MatlabPointCloud(filename, 'point_cloud')
+    p.ComputeDistanceMatrix()
+    # Create Selector
+    pointcloud_sel = sel.PointCloudSelector(p, 50, "RandomSelector")
+    R = float(pointcloud_sel.getdistance_subsetstoPointcloud()) / 2
+    # R = 0
+    print 'R = ', R
+    numdivision = 10
+    maxdim = 3
+    ws = WitnessStream(landmarkselector=pointcloud_sel, maxdistance=R, numdivision=numdivision, maxdimension=maxdim)
+    ws.construct()
+    print ws
 if __name__ == "__main__":
     # testpointcloud()
     # testMatlabPointcloud()
-    testRandomSelector()
+    # testRandomSelector()
     # testRandomSelectorDistanceMetricio()
+    # testMaxdist()
+    testWitnessStream()
