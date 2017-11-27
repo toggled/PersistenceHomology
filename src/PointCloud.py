be09c2573e8bb13f9e70606b3bbf57e3953ce94e
Naheed <naheedcse@gmail.com>
2017-11-27 18:08:06 +0800

    MatlabPointcloud class is now a subclass of Pointcloud

--- src/PointCloud.py
+++ src/PointCloud.py
@@ -3,7 +3,7 @@ from scipy import spatial
 import scipy.io
 
 
-class PointCloud:
+class PointCloud(object):
     def __init__(self, matrixofPoints):
         '''
         :param matrixofPoints: 2D Array or a List of Points (1D array).
@@ -18,31 +18,23 @@ class PointCloud:
 
     def ComputeDistanceMatrix(self):
         # Euclidean for now
-        self.distmat = spatial.distance.pdist(self.Points, metric='euclidean')
+        self.distmat = spatial.distance.squareform(spatial.distance.pdist(self.Points, metric='euclidean'))
 
 
-class MatlabPointCloud:
+class MatlabPointCloud(PointCloud):
     def __init__(self, matlabfilename, varname):
         """
         :param matlabfilename: A .mat file containing 2D matrix of Pointclouds. Points across row, dimension across column
         :param varname: Which variable from the .mat file contains the pointcloud
         """
+
         assert isinstance(matlabfilename, str)
         if not matlabfilename.endswith("mat", 0, len(matlabfilename)):
             raise Exception("Not a mat file")
-        self.Points = None
+        Points = None
         try:
-            self.Points = scipy.io.loadmat(matlabfilename)[varname]
+            Points = scipy.io.loadmat(matlabfilename)[varname]
         except:
             raise Exception('incorrect variable specified.')
 
-        self.size = self.Points.shape[0]
-        self.dimension = self.Points.shape[1]
-        self.distmat = None
-
-    def __getitem__(self, item):
-        return self.Points.__getitem__(item)
-
-    def ComputeDistanceMatrix(self):
-        # Euclidean for now
-        self.distmat = spatial.distance.pdist(self.Points, metric='euclidean')
+        super(MatlabPointCloud, self).__init__(Points)
