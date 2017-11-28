e389d73421c11e91815b85e0bc3a4f103e8943be
Naheed <naheedcse@gmail.com>
2017-11-28 23:38:45 +0800

    The input matrix squareness check added.

--- src/DistanceMetricinput.py
+++ src/DistanceMetricinput.py
@@ -6,7 +6,7 @@ import scipy.io
 class DistanceMetricIn:
     def __init__(self, matfile, varname, dimension):
         """
-        :param A matlab .mat file holding a 2D matrix
+        :param A matlab .mat file holding a square 2D matrix
         """
         assert isinstance(matfile, str)
         if not matfile.endswith("mat", 0, len(matfile)):
@@ -17,5 +17,7 @@ class DistanceMetricIn:
         except:
             raise Exception('incorrect variable specified.')
 
+        assert self.distance_matrix.shape[0] == self.distance_matrix.shape[
+            1]  # The matrix should be a square matrix i.e a valid distance matrix
         self.dim = dimension
         self.size = self.distance_matrix.shape[0]
