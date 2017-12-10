14bc9c9071c5a6a8842f66382e075d58f3562111
Naheed <naheedcse@gmail.com>
2017-12-11 03:37:51 +0800

    Some variable names changed according to PEP suggestions.

--- src/PointCloud.py
+++ src/PointCloud.py
@@ -1,40 +1,49 @@
+"""
+This module contains class definitions to deal Point Cloud data coming from different sources
+ (e.g. Matlab matrix, pointcloud from file, numpy matrix)
+"""
 import numpy as np
 from scipy import spatial
 import scipy.io
 
 
 class PointCloud(object):
-    def __init__(self, matrixofPoints):
+    def __init__(self, matrixofpoints):
         '''
-        :param matrixofPoints: 2D Array or a List of Points (1D array).
+        :param matrixofpoints: 2D Array or a List of points (1D array).
         '''
-        self.Points = np.array(matrixofPoints, dtype=float)
-        self.size = self.Points.shape[0]
-        self.dimension = self.Points.shape[1]
+        self.points = np.array(matrixofpoints, dtype=float)
+        self.size = self.points.shape[0]
+        self.dimension = self.points.shape[1]
         self.distmat = None
 
     def __getitem__(self, item):
-        return self.Points.__getitem__(item)
+        return self.points.__getitem__(item)
 
-    def ComputeDistanceMatrix(self):
-        # Euclidean for now
-        self.distmat = spatial.distance.squareform(spatial.distance.pdist(self.Points, metric='euclidean'))
+    def compute_distancematrix(self):
+        """
+        Computes Euclidean Distance Matrix (for now)
+        :rtype: None
+        """
+        self.distmat = spatial.distance.squareform(spatial.distance.pdist(self.points, metric='euclidean'))
 
 
 class MatlabPointCloud(PointCloud):
     def __init__(self, matlabfilename, varname):
         """
-        :param matlabfilename: A .mat file containing 2D matrix of Pointclouds. Points across row, dimension across column
+        Class for constructing  Point cloud data created in Matlab
+        :param matlabfilename: A .mat file containing 2D matrix of Pointclouds.
+                Points across row, dimension across column
         :param varname: Which variable from the .mat file contains the pointcloud
         """
 
         assert isinstance(matlabfilename, str)
         if not matlabfilename.endswith("mat", 0, len(matlabfilename)):
             raise Exception("Not a mat file")
-        Points = None
+        points = None
         try:
-            Points = scipy.io.loadmat(matlabfilename)[varname]
+            points = scipy.io.loadmat(matlabfilename)[varname]
         except:
             raise Exception('incorrect variable specified.')
 
-        super(MatlabPointCloud, self).__init__(Points)
+        super(MatlabPointCloud, self).__init__(points)
